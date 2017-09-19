b36e71de75f3063ecc0fdcdcc2d9ab310b591ab7
Tiago Alves <tiago.alves718@gmail.com>
2017-09-19 15:21:19 -0300

    - Adding city model to own location propertie - Remove location model, for a while

--- server/models/city.js
+++ server/models/city.js
@@ -1,15 +1,15 @@
 'use strict';
 
-module.exports = (Location) => {
+module.exports = (City) => {
       // define remote method
-  Location.getLocation = (data) => {
+  City.getCity = (data) => {
     return new Promise((resolve, reject) => {
       resolve(data);
     });
   };
 
       // expose remote method
-  Location.remoteMethod('getLocation', {
+  City.remoteMethod('getCity', {
     accepts: {arg: 'data', type: 'object', http: {source: 'body'}},
     returns: {arg: 'data', type: 'object', root: true},
     http: {path: '/find', verb: 'post'},
