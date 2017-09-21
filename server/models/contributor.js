72b4be4eed42a9bffd7a2664aad4e0c89a4ae784
Tiago Alves <tiago.alves718@gmail.com>
2017-09-21 09:47:09 -0300

    Change name of relation to contributor

--- server/models/contributor.js
+++ server/models/contributor.js
@@ -1,5 +1,6 @@
 'use strict';
 
-module.exports = (Collaborator) => {
-    
+module.exports = (Contributor) => {
+	console.log(Contributor);
+
 }
--- server/sql/scripts-db.sql
+++ server/sql/scripts-db.sql
@@ -1,65 +1,39 @@
 -- Create a new database called 'CooHomelessDB'
 USE CooHomelessDB;
-CREATE DATABASE CooHomelessDB
+CREATE DATABASE CooHomelessDB;
 
 -- Create a new table called 'ORGANIZATION'
-CREATE TABLE ORGANIZATION
-(
-    ID_ORGANIZATION INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY, -- primary key column
-    NOME [NVARCHAR](50) NOT NULL,
-    DESC_ORGANIZATION [NVARCHAR](100),
+CREATE TABLE ORGANIZATION (
+    ID_ORGANIZATION INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    NOME NVARCHAR(50) NOT NULL,
+    DESC_ORGANIZATION NVARCHAR(100),
     CNPJ INTEGER NOT NULL
-    -- specify more columns here
 );
-GO
 
 -- Create a new table called 'COLLABORATOR'
-CREATE TABLE COLLABORATOR
-(
-    ID_COLLABORATOR INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY, -- primary key column
-    DESC_COLLABORATOR [NVARCHAR](50) NOT NULL,
+CREATE TABLE COLLABORATOR (
+    ID_COLLABORATOR INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    DESC_COLLABORATOR NVARCHAR(50) NOT NULL,
     CPF INTEGER NOT NULL
-    -- specify more columns here
-);
-GO
-
-
--- Create a new table called 'METTING_POINT'
-CREATE TABLE METTING_POINT
-(
-    METTING_POINTId INT NOT NULL PRIMARY KEY, -- primary key column
-    Column1 [NVARCHAR](50) NOT NULL,
-    Column2 [NVARCHAR](50) NOT NULL
-    -- specify more columns here
-);
-GO
-create table METTING_POINT
-(
- ID_METTING_POINT integer auto_increment not null,
- ID_LOCALIZATION,
- ID_HOMELESS,
- -- 
 );
 
 
--- Create the table in the specified schema
-CREATE TABLE LOCALIZATION
-(
-    ID_LOCALIZATION INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY, -- primary key column
-    LATITUDE [NVARCHAR](50) NOT NULL,
-    LONGETUDE [NVARCHAR](50) NOT NULL
-    -- specify more columns here
+-- Create a new table called 'LOCALIZATION'
+CREATE TABLE LOCALIZATION (
+    ID_LOCALIZATION INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    ID_HOMELESS INTEGER NOT NULL,
+    LATITUDE NVARCHAR(50) NOT NULL,
+    LONGETUDE NVARCHAR(50) NOT NULL
 );
-GO
 
-create table homeless(
- id_homeless integer auto_increment not null
+CREATE TABLE homeless (
+    id_homeless INTEGER AUTO_INCREMENT NOT NULL
 );
  
-create table donation(
- id_homeless integer auto_increment not null
+CREATE TABLE donation (
+    id_donation INTEGER AUTO_INCREMENT NOT NULL
 );
 
-create table city(
- id_city integer auto_increment not null
- );
+CREATE TABLE city (
+    id_city INTEGER AUTO_INCREMENT NOT NULL
+);
