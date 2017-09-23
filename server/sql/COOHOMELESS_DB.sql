c545ee3ae59b04dd6f9c7c633fa5fa82d1ae48ff
Tiago Alves <tiago.alves718@gmail.com>
2017-09-23 05:22:40 -0300

    Refactor database sripts - Refactor create database -  Add create_at to metting of ONG and Contribuitor

---
+++ server/sql/COOHOMELESS_DB.sql
@@ -0,0 +1,4 @@
+-- Create a new database called 'COO_HOMELESS_DB'
+-- Connect to the 'master' database to run this snippet
+CREATE DATABASE IF NOT EXISTS COOHOMELESS_DB;
+USE COOHOMELESS_DB;
--- server/sql/DB-CREATE.sql
+++
@@ -1,3 +0,0 @@
--- Create a new database called 'CooHomelessDB'
-CREATE DATABASE COOHOMELESS_DB;
-USE COOHOMELESS_DB;
