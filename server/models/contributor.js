e5216fa3d760f247c573564fe8a6e942f08d92bd
Tiago Alves <tiago.alves718@gmail.com>
2017-09-23 05:25:30 -0300

    Refactoring models structure - Separete concerns in folders to each model - Move contribuitor and ong to folders - Create others models folders - Remove relation on contribuitor to localization - Add some defaultData to contribuitor model

--- server/models/contributor.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-module.exports = (Contributor) => {
-	console.log(Contributor);
-
-}
---
+++ server/models/contributor/contributor.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = (Contributor) => {
+
+	Contributor.defaultData = [
+		{
+			"id_contributor": 0,
+			"name": "Josnelson",
+			"cpf": "12345678910"
+		}
+	];
+}
