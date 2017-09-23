d20721df9beffa1cebdd392c36add8336265c7a4
Tiago Alves <tiago.alves718@gmail.com>
2017-09-23 05:19:16 -0300

    Updating load-model-default-data.config - Alterating name of file - Refactoring to ES6 sintax - Avoid callback hell with arrow functions

---
+++ server/boot/load-model-default-data.config.js
@@ -0,0 +1,34 @@
+import async from 'async';
+
+module.exports = (app) => {
+	let models = [];
+	let calls = [];
+
+	models.push(app.models.contributor);
+
+	if (models.length > 0) {
+		models[0].dataSource.setMaxListeners(0);
+		
+		models.forEach((model) => {
+			if (model.defaultData) {
+				model.defaultData.forEach((value) => {
+					calls.push(addDefaultData(model, value));
+				});
+			}
+		});
+	}
+
+	async.parallel(calls, (err, data) => {
+		if (err) {
+			console.log(err);
+		}
+	});
+}
+
+let addDefaultData = (model, value) => {
+	return (cb) => {
+		model.create(value, (err) => {
+			if (err) console.log('Error:', err);
+		});
+	};
+}
