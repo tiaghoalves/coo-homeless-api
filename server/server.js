540729e916d1f35750a8140fcad4dd07057ccd69
Tiago Alves <tiago.alves718@gmail.com>
2017-09-25 22:44:28 -0300

    Convertendo es5 to es6

--- server/server.js
+++ server/server.js
@@ -1,29 +1,27 @@
 'use strict';
 
-var loopback = require('loopback');
-var boot = require('loopback-boot');
+import loopback from 'loopback';
+import boot from 'loopback-boot';
 
-var app = module.exports = loopback();
+const app = module.exports = loopback();
 
-app.start = function() {
-  // start the web server
-  return app.listen(function() {
-    app.emit('started');
-    var baseUrl = app.get('url').replace(/\/$/, '');
-    console.log('Web server listening at: %s', baseUrl);
-    if (app.get('loopback-component-explorer')) {
-      var explorerPath = app.get('loopback-component-explorer').mountPath;
-      console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
-    }
-  });
-};
+app.start = () => // start the web server
+	app.listen(() => {
+		app.emit('started');
+		const baseUrl = app.get('url').replace(/\/$/, '');
+		console.log('Web server listening at: %s', baseUrl);
+		if (app.get('loopback-component-explorer')) {
+			const explorerPath = app.get('loopback-component-explorer').mountPath;
+			console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
+		}
+	});
 
 // Bootstrap the application, configure models, datasources and middleware.
 // Sub-apps like REST API are mounted via boot scripts.
-boot(app, __dirname, function(err) {
-  if (err) throw err;
+boot(app, __dirname, err => {
+	if (err) throw err;
 
-  // start the server if `$ node server.js`
-  if (require.main === module)
-    app.start();
+	// start the server if `$ node server.js`
+	if (require.main === module)
+		app.start();
 });
