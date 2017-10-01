6956538b41ccbeca5f018157da33b5d6b874d075
Henrique Bitencourt <henrique55595@gmail.com>
2017-10-01 20:17:13 -0300

    add changes

---
+++ common/models/meeting/meeting.js
@@ -0,0 +1,12 @@
+'use strict';
+
+export default (Organization) =>  {
+    Organization.defaultData = [
+		{
+			"id_organization": 0,
+            "name": "Centro POP",
+            "desc_organization": "Centro de Referência Especializado para População em Situação de Rua",
+			"cnpj": "12345678910123"
+		}
+	];
+};
