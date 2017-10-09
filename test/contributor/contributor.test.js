be6474f4b20486c22ad010dc3831d30f0ba76566
Tiago Alves <tiago.alves718@gmail.com>
2017-10-08 22:24:39 -0300

    Refatorando testes - Trocando then() para chamar end(callback) - Retirando authorization token nos testes do Contributor - Padronizar ES5 para os casos de testes

--- test/contributor/contributor.test.js
+++ test/contributor/contributor.test.js
@@ -30,40 +30,37 @@ describe('Contributor', function() {
         });
     });
 
-    describe('GET', function() {
-        it('GET should get all Contributors', (done) => {
+    describe('GET /contributors', function() {
+        it('GET should get all Contributors', function(done) {
             api.get('/contributors')
-                .set('authorization', 'sLU7urJMISrSbKmysDwndNalayIHymkFdbkQIFLijTAMkVUGsD3Dqfw9guinqTjO')
                 .expect('Content-Type', /json/)
                 .expect(200)
-                .then(function(res) {
-                    // if (err) return done(err);
+                .end(function(err, res) {
+                    if (err) return done(err);
                     var contributor = res.body;
                     expect(contributor.length).to.be.above(1);
                     done();
                 });
         });
 
-        it('GET should get all contributors and expect not to be 1 or 2', (done) => {
+        it('GET should get all contributors and expect not to be 1 or 2', function(done) {
             api.get('/contributors')
-                .set('authorization', 'sLU7urJMISrSbKmysDwndNalayIHymkFdbkQIFLijTAMkVUGsD3Dqfw9guinqTjO')
                 .expect('Content-Type', /json/)
                 .expect(200)
-                .then(function(res) {
-                    // if (err) return done(err);
+                .end(function(err, res) {
+                    if (err) return done(err);
                     var contributor = res.body;
                     expect(contributor).to.not.equal([1, 2]);
                     done();
                 });
         });
 
-        it('GET should get all contributors expect to be Array', (done) => {
+        it('GET should get all contributors expect to be Array', function(done) {
             api.get('/contributors')
-                .set('authorization', 'sLU7urJMISrSbKmysDwndNalayIHymkFdbkQIFLijTAMkVUGsD3Dqfw9guinqTjO')
                 .expect('Content-Type', /json/)
                 .expect(200)
-                .then(function(res) {
-                    // if (err) return done(err);
+                .end(function(err, res) {
+                    if (err) return done(err);
                     var contributor = res.body;
                     expect(contributor).to.be.instanceof(Array);
                     done();
--- test/user/user.test.js
+++ test/user/user.test.js
@@ -5,11 +5,11 @@ var supertest = require('supertest');
 var assert = require('assert');
 var api = supertest('http://localhost:3000/api');
 
-describe('Authentication User', function() {
+describe('User Authentication', function() {
 
     it('findOne user from model successfully', function() {
         api.get('/users/findOne')
-            .set('authorization', 'sLU7urJMISrSbKmysDwndNalayIHymkFdbkQIFLijTAMkVUGsD3Dqfw9guinqTjO')
+            .set('authorization', 'nWQLX6qu68nEDmRg5AUIHfYcqMfWDx6ieAl7CWjOJNU78so3Ftb6MZcCDJKKJq6t')
             .expect(200)
             .then(function (res) {
                 expect(res.body).to.be.instanceof(Object);
