e5fe6785fe56b312d14688fb3b1f05f459f74920
Tiago Alves <tiago.alves718@gmail.com>
2017-11-17 22:25:41 -0200

    remove ssl configs

--- server/server.js
+++ server/server.js
@@ -3,40 +3,22 @@
 import loopback from 'loopback';
 import boot from 'loopback-boot';
 
-import http from 'http';
-import https from 'https';
-import sslConfig from './ssl-config';
+// import http from 'http';
+// import https from 'https';
+// import sslConfig from './ssl-config';
 
 const app = module.exports = loopback();
 // start the web server
-app.start = (httpOnly) => {
-	if (httpOnly === undefined) {
-		httpOnly = process.env.HTTP;
-	}
-	let server = null;
-
-	if (!httpOnly) {
-		let options = {
-			key: sslConfig.privateKey,
-			cert: sslConfig.certificate,
-		};
-		server = https.createServer(options, app);
-	} else {
-		server = http.createServer(app);
-	}
-	
-	server.listen(app.get('port'), () => {
-		const baseUrl = (httpOnly ? 'http://' : 'https://') + app.get('host') + ':' + app.get('port');
-		app.emit('started', baseUrl);
-		console.log('Web server listening at: %s%s', baseUrl, '/');
-		
+app.start = () => {
+	app.listen(() => {
+		app.emit('started');
+		const baseUrl = app.get('url').replace(/\/$/, '');
+		console.log('Web server listening at: %s', baseUrl);
 		if (app.get('loopback-component-explorer')) {
 			const explorerPath = app.get('loopback-component-explorer').mountPath;
 			console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
 		}
 	});
-
-	return server;
 }
 // Bootstrap the application, configure models, datasources and middleware.
 // Sub-apps like REST API are mounted via boot scripts.
